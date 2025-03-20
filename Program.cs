0db009d663db0ed4b9b52cfbb23f54f04d940144
MOHAMMED <TOM@20377@GMAIL.COM>
2025-03-20 10:56:36 +0400

    creat the setup

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace SimpleStudentManagementProject_CSharpProject1
 {
-    internal class Program
+    internal class Progra
     {
+        static double[] studentMarks = new double[10];
+        static string[] studentNames = new string[10];
+        static int studentCount = 0;
+        static int[] age = new int[10];
+        static DateTime[] dates  = new DateTime[10];
+      
+
+   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
